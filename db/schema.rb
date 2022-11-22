084f9417f017d1012158edd90f520f819094fb69
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2022-11-22 21:42:19 +0500

    Application: Added Suppliers Model and Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_22_161951) do
+ActiveRecord::Schema.define(version: 2022_11_22_164110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,12 @@ ActiveRecord::Schema.define(version: 2022_11_22_161951) do
     t.integer "price_per_unit", default: 0, null: false
     t.integer "bulk_price", default: 0, null: false
     t.integer "retail_price", default: 0, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
