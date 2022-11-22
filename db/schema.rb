207c65e443b479a3de7c2e51bf850903f19e02d6
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2022-11-22 21:18:42 +0500

    Application: Added Brands Model and Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_22_160707) do
+ActiveRecord::Schema.define(version: 2022_11_22_161629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "brands", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.string "customer_name", default: "", null: false
