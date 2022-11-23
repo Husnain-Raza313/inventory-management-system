c59cd92ef4ab6a9774f19f233825f89eb149da73
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2022-11-23 22:21:21 +0500

    Application: Added ProductSuppliers Table with recommended Changes

--- db/migrate/20221123171921_create_product_suppliers.rb
+++ db/migrate/20221123171921_create_product_suppliers.rb
@@ -1,11 +1,8 @@
-# frozen_string_literal: true
-
 class CreateProductSuppliers < ActiveRecord::Migration[6.1]
   def change
     create_table :product_suppliers do |t|
       t.references :product, null: false, foreign_key: true
       t.references :supplier, null: false, foreign_key: true
-
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_23_171425) do
+ActiveRecord::Schema.define(version: 2022_11_23_171921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,15 @@ ActiveRecord::Schema.define(version: 2022_11_23_171425) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "product_suppliers", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.bigint "supplier_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_product_suppliers_on_product_id"
+    t.index ["supplier_id"], name: "index_product_suppliers_on_supplier_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -102,6 +111,8 @@ ActiveRecord::Schema.define(version: 2022_11_23_171425) do
   add_foreign_key "order_items", "orders"
   add_foreign_key "order_items", "products"
   add_foreign_key "orders", "users"
+  add_foreign_key "product_suppliers", "products"
+  add_foreign_key "product_suppliers", "suppliers"
   add_foreign_key "products", "brands"
   add_foreign_key "products", "users"
   add_foreign_key "transactions", "orders"
