fd2c29375272dfa3d80a5caa6061ade315208925
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2022-11-23 22:00:49 +0500

    Application: Added Transactions Table with recommended Changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_23_165243) do
+ActiveRecord::Schema.define(version: 2022_11_23_165858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2022_11_23_165243) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.integer "order_status", default: 0, null: false
+    t.bigint "order_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_transactions_on_order_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,4 +52,5 @@ ActiveRecord::Schema.define(version: 2022_11_23_165243) do
   end
 
   add_foreign_key "orders", "users"
+  add_foreign_key "transactions", "orders"
 end
