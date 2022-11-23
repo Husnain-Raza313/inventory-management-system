e821b2a9d5e333a7d8439873b6481c7a7e60230d
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2022-11-23 22:09:03 +0500

    Application: Added Suppliers Table with recommended Changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_23_170541) do
+ActiveRecord::Schema.define(version: 2022_11_23_170734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,12 @@ ActiveRecord::Schema.define(version: 2022_11_23_170541) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string "name", limit: 30, default: "", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "transactions", force: :cascade do |t|
