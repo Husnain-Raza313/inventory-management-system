20b2774dd69bceb0101c6d53ce415c8fd6b63c2e
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2022-11-23 22:04:37 +0500

    Application: Added Categories Table with recommended Changes

--- db/migrate/20221123170250_create_categories.rb
+++ db/migrate/20221123170250_create_categories.rb
@@ -1,9 +1,7 @@
-# frozen_string_literal: true
-
 class CreateCategories < ActiveRecord::Migration[6.1]
   def change
     create_table :categories do |t|
-      t.string :name, null: false, default: ''
+      t.string :name, null: false, default: '', limit: 30
       t.timestamps
     end
   end
