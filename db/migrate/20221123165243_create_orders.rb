42b21d047efc82a6acca02b725598e541676168f
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2022-11-23 21:56:22 +0500

    Application: Added Orders Table with recommended Changes

--- db/migrate/20221123165243_create_orders.rb
+++ db/migrate/20221123165243_create_orders.rb
@@ -1,12 +1,12 @@
-# frozen_string_literal: true
-
 class CreateOrders < ActiveRecord::Migration[6.1]
   def change
     create_table :orders do |t|
-      t.string :customer_name, null: false, default: ''
+      t.string :customer_name, null: false, default: '', limit: 30
       t.string :customer_address
-      t.string :phone_no, null: false, default: ''
+      t.string :phone_no, null: false, default: '', limit: 15
       t.integer :total_price, null: false, default: 0
+      t.references :user, null: false, foreign_key: true
+
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_23_164623) do
+ActiveRecord::Schema.define(version: 2022_11_23_165243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.string "customer_name", limit: 30, default: "", null: false
+    t.string "customer_address"
+    t.string "phone_no", limit: 15, default: "", null: false
+    t.integer "total_price", default: 0, null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", limit: 30, default: "", null: false
@@ -32,4 +43,5 @@ ActiveRecord::Schema.define(version: 2022_11_23_164623) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "orders", "users"
 end
