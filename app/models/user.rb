574332d841c626f168f1db230ad49d1cc4c02cab
Hammad538 <hammad.rashid@devsinc.com>
2022-11-28 14:34:52 +0500

    Error Message:: Moved error message in translation file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,11 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  VALID_EMAIL_REGEX= /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
-  validates_presence_of :username
-  validates_uniqueness_of :username
-  validates_presence_of :image
-  validates_presence_of :description
-  validates_length_of :description, :minimum => 10, :maximum => 30
-  validates :email , presence: true,uniqueness: { case_sensetive:false }, format:{ with:VALID_EMAIL_REGEX,multiline:true }
+  VALID_EMAIL_REGEX = /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i.freeze
+  validates :username, :image, :description, :email, presence: true
+  validates :username, :email, uniqueness: true
+  validates :description, length: { minimum: 10, maximum: 30 }
+  validates :email, format: { with: VALID_EMAIL_REGEX, multiline: true }
   validate :image_type
 
   devise :database_authenticatable, :registerable,
@@ -17,12 +15,12 @@ class User < ApplicationRecord
   has_many :products, dependent: :destroy
   has_one_attached :image, dependent: :destroy
 
-private
+  private
 
-    def image_type
-    errors.add(:image, "is missing!") and return if image.attached? == false
-    if !image.content_type.in?(%('image/jpeg image/png image/heic'))
-      errors.add(:image, "needs to be a jpeg or png!")
-      end
-    end
+  def image_type
+    return if image.attached? == false
+    return if image.content_type.in?(%('image/jpeg image/png image/heic'))
+
+    errors.add(:image, I18n.t('devise.registrations.image'))
+  end
 end
