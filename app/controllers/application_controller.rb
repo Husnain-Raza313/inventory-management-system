7eccc0091f7e5efaac9e36caf0d988b13022fcaf
Hammad538 <hammad.rashid@devsinc.com>
2022-11-28 14:34:01 +0500

    Feedback:: Resolved issues related to syntax and spacing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,18 +4,18 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   protect_from_forgery with: :exception
   add_flash_types :danger, :info, :warning, :success, :messages
-  
+
   before_action :configure_permitted_parameters, if: :devise_controller?
   layout :set_layout
+
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :image, :description])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name, :image, :description])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username name image description])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[username name image description])
   end
 
   def set_layout
-    current_user ? 'application': 'auth'
+    current_user ? 'application' : 'auth'
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :home, only: [:index]
+  resources :home, only: %i[index]
   devise_for :users
   authenticated :user do
-    root to: "home#index", as: :admin_route
+    root to: 'home#index', as: :admin_route
   end
-  root to: "home#index" 
+  root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
