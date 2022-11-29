0075b6c5bd13f4716124098f12563fc30bbaabeb
Hammad538 <hammad.rashid@devsinc.com>
2022-11-29 17:44:42 +0500

    Product-Categories:: Implemented CRUD actions of product categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class Category < ApplicationRecord
+  has_one_attached :image
+  validates :name, presence: true, length: { maximum: 30, minimum: 1 }
+  validates :name, uniqueness: true
+  validates :description, length: { minimum: 10, maximum: 30 }
+  validates :image, presence: true,
+                    blob: { content_type: ['image/png', 'image/jpg', 'image/jpeg'], size_range: 1..(5.megabytes) }
   has_many :category_products, dependent: :destroy
   has_many :products, through: :category_products
 end
