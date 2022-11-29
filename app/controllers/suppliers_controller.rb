5c3fb98e4e2cf0e7500153192be931eeb7b2a0d6
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2022-11-29 15:39:57 +0500

    Application: Added Suppliers Controller, Views, Test and Views

---
+++ app/controllers/suppliers_controller.rb
@@ -0,0 +1,61 @@
+class SuppliersController < ApplicationController
+  before_action :set_supplier, only: %i[ show edit update destroy ]
+
+  def index
+    @suppliers = Supplier.all
+  end
+
+  def show
+  end
+
+  def new
+    @supplier = Supplier.new
+  end
+
+  def edit
+  end
+
+  def create
+    @supplier = Supplier.new(supplier_params)
+
+    respond_to do |format|
+      if @supplier.save
+        format.html { redirect_to supplier_url(@supplier), notice: "Supplier was successfully created." }
+        format.json { render :show, status: :created, location: @supplier }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @supplier.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @supplier.update(supplier_params)
+        format.html { redirect_to supplier_url(@supplier), notice: "Supplier was successfully updated." }
+        format.json { render :show, status: :ok, location: @supplier }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @supplier.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @supplier.destroy
+
+    respond_to do |format|
+      format.html { redirect_to suppliers_url, notice: "Supplier was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_supplier
+      @supplier = Supplier.find_by(params[:id])
+    end
+
+    def supplier_params
+      params.require(:supplier).permit(:name, :supplier_address, :phone_no)
+    end
+end
