0e86a8ebaab5069f697fbf9d4d8363c192fb82f5
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2022-11-30 14:30:47 +0500

    Helpers: Placed Suppliers helper code into application helper file

--- app/helpers/suppliers_helper.rb
+++ app/helpers/suppliers_helper.rb
@@ -1,9 +1,2 @@
 module SuppliersHelper
-  def image_view(supplier)
-    if supplier.image.filename.nil?
-      image_tag('no-image.jpeg', size: '500x250', alt: 'Supplier Image', class: ' w-100 rounded-top')
-    else
-      image_tag(supplier.image, size: '500x250', alt: 'Supplier Image', class: ' w-100 rounded-top')
-    end
-  end
 end
