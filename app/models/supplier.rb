2f8297f892d5ed9a56fa1616e0ab330598f5507f
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2022-12-01 16:26:43 +0500

    SupplierModel: Added constants for image size and types

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -6,7 +6,8 @@ class Supplier < ApplicationRecord
   has_one_attached :image
 
   validates :name, presence: true, length: { maximum: 30, minimum: 1 }
-  # validates :image, presence: true, blob: { content_type: ['image/png', 'image/gif', 'image/jpg', 'image/jpeg'], size_range: 1..(5.megabytes) }
+  validates :image, presence: true,
+                    blob: { content_type: ALLOWED_IMAGE_TYPES, size_range: ALLOWED_IMAGE_SIZE }
   validates :phone_no, presence: true, length: { maximum: 15}
   validates :supplier_address, presence: true, length: { minimum: 10}
 end
