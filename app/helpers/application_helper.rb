4039760ab336b2da7ca5c4eb4e9d198e6e36a9fb
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2022-12-01 20:55:04 +0500

    Application: Applied Rubocop and refactored translation keys

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,12 +4,8 @@ module ApplicationHelper
 
   IMAGE_SIZE = ('500x250').freeze
   def image_view(obj)
-    if obj.image.filename.nil?
-      image_tag('no-image.jpeg', size: ApplicationHelper::IMAGE_SIZE, alt: t('image', param: "#{obj.class.name}"),
-                                 class: ' w-100 rounded-top')
-    else
-      image_tag(obj.image, size: ApplicationHelper::IMAGE_SIZE, alt: t('image', param: "#{obj.class.name}"),
-                           class: ' w-100 rounded-top')
-    end
+    return image_tag(obj.image, size: ApplicationHelper::IMAGE_SIZE, alt: t('image', param: "#{obj.class.name}"), class: ' w-100 rounded-top') if obj.image.attached?
+
+    image_tag('no-image.jpeg', size: ApplicationHelper::IMAGE_SIZE, alt: t('image', param: "#{obj.class.name}"), class: ' w-100 rounded-top')
   end
 end
