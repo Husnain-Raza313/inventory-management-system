7717026b3e0849565e97e525fafcc1ae89c6bfb5
Hammad538 <hammad.rashid@devsinc.com>
2022-12-02 11:22:17 +0500

    Images:: Feedback addressed related to image helper

--- app/helpers/brands_helper.rb
+++ app/helpers/brands_helper.rb
@@ -1,13 +1,5 @@
 # frozen_string_literal: true
 
 module BrandsHelper
-  def image_view(brand)
-    if brand.image.filename.nil?
-      image_tag('no-image.jpeg', size: ApplicationHelper::IMAGE_SIZE, alt: t('image', param: 'Brand'),
-                                 class: ' w-100 rounded-top')
-    else
-      image_tag(brand.image, size: ApplicationHelper::IMAGE_SIZE, alt: t('image', param: 'Brand'),
-                             class: ' w-100 rounded-top')
-    end
-  end
+
 end
--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,11 +1,3 @@
 module CategoriesHelper
-  def image_view(category)
-    if category.image.filename.nil?
-      image_tag('no-image.jpeg', size: ApplicationHelper::IMAGE_SIZE, alt: t('image', param: 'Category'),
-                                 class: ' w-100 rounded-top')
-    else
-      image_tag(category.image, size: ApplicationHelper::IMAGE_SIZE, alt: t('image', param: 'Category'),
-                                class: ' w-100 rounded-top')
-    end
-  end
+
 end
