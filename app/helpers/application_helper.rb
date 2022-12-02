003383bdeee674cd33dc285068fe6bb03f4b0365
Hammad538 <hammad.rashid@devsinc.com>
2022-12-02 15:30:58 +0500

    Image:: feedback addressed related to hardcoded alt-image-name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,10 @@ module ApplicationHelper
   IMAGE_SIZE = '500x250'
   def image_view(obj)
     if obj.image.filename.nil?
-      image_tag('no-image.jpeg', size: ApplicationHelper::IMAGE_SIZE, alt: t('image', param: 'Brand'),
+      image_tag('no-image.jpeg', size: ApplicationHelper::IMAGE_SIZE, alt: t('image', param: "#{obj.class.name}"),
                                  class: ' w-100 rounded-top')
     else
-      image_tag(obj.image, size: ApplicationHelper::IMAGE_SIZE, alt: t('image', param: 'Brand'),
+      image_tag(obj.image, size: ApplicationHelper::IMAGE_SIZE, alt: t('image', param: "#{obj.class.name}"),
                            class: ' w-100 rounded-top')
     end
   end
