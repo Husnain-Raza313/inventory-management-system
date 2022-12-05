2186ab1317ef82583e713fe18a2df216bc372c99
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2022-12-05 19:01:57 +0500

    Applicaiton: Added em tag to flash message

--- app/javascript/packs/flash.js
+++ app/javascript/packs/flash.js
@@ -1,5 +1,5 @@
 $(function(){
   $('#flashes').children('div')?.each((index, child)=>{
-    toastr[$(child).attr('class')]($(child).text())
+    toastr[$(child).attr('class')]($(child).html())
   })
 })
