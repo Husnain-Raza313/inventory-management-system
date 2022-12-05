d7c55d4d3b652e4702a1fc23ecde296be00b4581
Hammad538 <hammad.rashid@devsinc.com>
2022-12-05 18:05:38 +0500

    Application:: Feedback addressed related to image method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  IMAGE_SIZE = '500x250'
+
+  IMAGE_SIZE = ('500x250').freeze
   def image_view(obj)
     if obj.image.filename.nil?
       image_tag('no-image.jpeg', size: ApplicationHelper::IMAGE_SIZE, alt: t('image', param: "#{obj.class.name}"),
