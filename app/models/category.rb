3bd875966676ad07b2491629b8c25e4540af8d6e
Hammad538 <hammad.rashid@devsinc.com>
2022-12-05 18:06:30 +0500

    Feedback:: Followed convention of writing validations and association

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 class Category < ApplicationRecord
-  has_one_attached :image
   validates :name, presence: true, length: { maximum: 30, minimum: 1 }
   validates :name, uniqueness: true
   validates :description, length: { minimum: 10, maximum: 30 }
   validates :image, presence: true,
                     blob: { content_type: ALLOWED_IMAGE_TYPES, size_range: ALLOWED_IMAGE_SIZE }
+
+  has_one_attached :image
   has_many :category_products, dependent: :destroy
   has_many :products, through: :category_products
 end
