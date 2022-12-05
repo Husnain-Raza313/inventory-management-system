ddcb3ef2350b5ecc8fa940c536683e8ef0d0be85
Hammad538 <hammad.rashid@devsinc.com>
2022-12-05 17:55:03 +0500

    Products:: Handled Categories and Suppliers

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: %i[show edit update destroy]
+  before_action :set_brand, only: %i[create]
+  before_action :assign_categories_and_supplier_to_product, only: %i[create]
 
   def index
     @products = Product.all
@@ -10,13 +12,13 @@ class ProductsController < ApplicationController
   end
 
   def create
-    @product = Product.new(product_params)
-
+    @product = @brand.products.new(product_params)
     if @product.save
-      flash[:success] = t('product.create.success')
+      flash[:success] = t('create.success', param: 'Product')
       redirect_to product_url(@product)
     else
-      render :new, status: :unprocessable_entity
+      flash[:warning] = @product.errors
+      redirect_to new_product_url(@product)
     end
   end
 
@@ -25,30 +27,59 @@ class ProductsController < ApplicationController
 
   def update
     if @product.update(product_params)
-      flash[:success] = t('product.update.success')
+      flash[:success] = t('update.success', param: 'Product')
       redirect_to product_url(@product)
     else
-      render :edit, status: :unprocessable_entity
+      flash[:warning] = @product.errors
+      redirect_to edit_product_url(@product)
     end
   end
 
   def destroy
     if @product.destroy
-      flash[:success] = t('product.destroy.success')
+      flash[:success] = t('destroy.success', param: 'Product')
     else
       flash[:danger] = @product.errors.full_messages.to_sentence
     end
+
     redirect_to product_url
   end
 
   private
 
   def set_product
-    @product = Product.find_by(id: params[:id])
+    @product = Product.find(params[:id])
+  end
+
+  def set_brand
+    @brand = Brand.find(params[:product][:brand_id])
+  end
+
+  def remove_empty_array_elements
+    @product_category = params[:product][:category_id].reject { |e| e.nil? || e&.empty? }
+    @product_supplier = params[:product][:suppler_id].reject { |e| e.nil? || e&.empty? }
+    [@product_category, product_supplier]
+  end
+
+  def assign_categories_and_supplier_to_product
+    remove_white_spaces if params[:product][:category_id].present? && params[:product][:suppler_id].present?
+    if @product_category.present? && product_supplier.present?
+      @product_category.each do |_product_category|
+        @category = Category.find(:product_category)
+        @product.categories << @category
+        @product_supplier.each do |_product_supplier|
+          @supplier = Supplier.find(:product_supplier)
+          @product.suppliers << @supplier
+        end
+      end
+    else
+      flash[:danger] = 'Select Category and Supplier'
+      redirect_to new_product_url
+    end
   end
 
   def product_params
-    params.require(:product).permit(:serial_no, :name, :description, :image, :quantity, :price_per_unit, :bulk_price,
-                                    :retail_price)
+    params.require(:product).permit(:serial_no, :name, :description, :image, :quantity, :price_per_unit,
+                                    :retail_price, :bulk_price, :brand_id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,174 +10,171 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_29_151120) do
-
+ActiveRecord::Schema.define(version: 20_221_129_151_120) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  create_table 'active_storage_attachments', force: :cascade do |t|
+    t.string 'name', null: false
+    t.string 'record_type', null: false
+    t.bigint 'record_id', null: false
+    t.bigint 'blob_id', null: false
+    t.datetime 'created_at', null: false
+    t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
+    t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness',
+                                                    unique: true
   end
 
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  create_table 'active_storage_blobs', force: :cascade do |t|
+    t.string 'key', null: false
+    t.string 'filename', null: false
+    t.string 'content_type'
+    t.text 'metadata'
+    t.string 'service_name', null: false
+    t.bigint 'byte_size', null: false
+    t.string 'checksum', null: false
+    t.datetime 'created_at', null: false
+    t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
   end
 
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  create_table 'active_storage_variant_records', force: :cascade do |t|
+    t.bigint 'blob_id', null: false
+    t.string 'variation_digest', null: false
+    t.index %w[blob_id variation_digest], name: 'index_active_storage_variant_records_uniqueness', unique: true
   end
 
-  create_table "brands", force: :cascade do |t|
-    t.string "name", limit: 30, default: "", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.text "description"
+  create_table 'brands', force: :cascade do |t|
+    t.string 'name', limit: 30, default: '', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.text 'description'
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name", limit: 30, default: "", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.text "description"
-    t.string "image"
+  create_table 'categories', force: :cascade do |t|
+    t.string 'name', limit: 30, default: '', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.text 'description'
+    t.string 'image'
   end
 
-  create_table "category_products", force: :cascade do |t|
-    t.bigint "category_id", null: false
-    t.bigint "product_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["category_id"], name: "index_category_products_on_category_id"
-    t.index ["product_id"], name: "index_category_products_on_product_id"
+  create_table 'category_products', force: :cascade do |t|
+    t.bigint 'category_id', null: false
+    t.bigint 'product_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['category_id'], name: 'index_category_products_on_category_id'
+    t.index ['product_id'], name: 'index_category_products_on_product_id'
   end
 
-  create_table "order_items", force: :cascade do |t|
-    t.integer "quantity", default: 0, null: false
-    t.integer "total_price", default: 0, null: false
-    t.bigint "product_id", null: false
-    t.bigint "order_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["order_id"], name: "index_order_items_on_order_id"
-    t.index ["product_id"], name: "index_order_items_on_product_id"
+  create_table 'order_items', force: :cascade do |t|
+    t.integer 'quantity', default: 0, null: false
+    t.integer 'total_price', default: 0, null: false
+    t.bigint 'product_id', null: false
+    t.bigint 'order_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['order_id'], name: 'index_order_items_on_order_id'
+    t.index ['product_id'], name: 'index_order_items_on_product_id'
   end
 
-  create_table "orders", force: :cascade do |t|
-    t.string "customer_name", limit: 30, default: "", null: false
-    t.string "customer_address"
-    t.string "phone_no", limit: 15, default: "", null: false
-    t.integer "total_price", default: 0, null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_orders_on_user_id"
+  create_table 'orders', force: :cascade do |t|
+    t.string 'customer_name', limit: 30, default: '', null: false
+    t.string 'customer_address'
+    t.string 'phone_no', limit: 15, default: '', null: false
+    t.integer 'total_price', default: 0, null: false
+    t.bigint 'user_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['user_id'], name: 'index_orders_on_user_id'
   end
 
-  create_table "product_suppliers", force: :cascade do |t|
-    t.bigint "product_id", null: false
-    t.bigint "supplier_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["product_id"], name: "index_product_suppliers_on_product_id"
-    t.index ["supplier_id"], name: "index_product_suppliers_on_supplier_id"
+  create_table 'product_suppliers', force: :cascade do |t|
+    t.bigint 'product_id', null: false
+    t.bigint 'supplier_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['product_id'], name: 'index_product_suppliers_on_product_id'
+    t.index ['supplier_id'], name: 'index_product_suppliers_on_supplier_id'
   end
 
-  create_table "products", force: :cascade do |t|
-    t.string "serial_no", limit: 30, default: "", null: false
-    t.string "name", limit: 30, default: "", null: false
-    t.text "description"
-    t.string "image"
-    t.integer "quantity", default: 0, null: false
-    t.integer "sold_quantity", default: 0, null: false
-    t.boolean "available", default: true, null: false
-    t.integer "price_per_unit", default: 0, null: false
-    t.integer "bulk_price", default: 0, null: false
-    t.integer "retail_price", default: 0, null: false
-    t.bigint "user_id", null: false
-    t.bigint "brand_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["brand_id"], name: "index_products_on_brand_id"
-    t.index ["user_id"], name: "index_products_on_user_id"
+  create_table 'products', force: :cascade do |t|
+    t.string 'serial_no', limit: 30, default: '', null: false
+    t.string 'name', limit: 30, default: '', null: false
+    t.text 'description'
+    t.string 'image'
+    t.integer 'quantity', default: 0, null: false
+    t.integer 'sold_quantity', default: 0, null: false
+    t.boolean 'available', default: true, null: false
+    t.integer 'price_per_unit', default: 0, null: false
+    t.integer 'bulk_price', default: 0, null: false
+    t.integer 'retail_price', default: 0, null: false
+    t.bigint 'brand_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['brand_id'], name: 'index_products_on_brand_id'
   end
 
-  create_table "roles", force: :cascade do |t|
-    t.string "name"
-    t.string "resource_type"
-    t.bigint "resource_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
-    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource"
+  create_table 'roles', force: :cascade do |t|
+    t.string 'name'
+    t.string 'resource_type'
+    t.bigint 'resource_id'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index %w[name resource_type resource_id], name: 'index_roles_on_name_and_resource_type_and_resource_id'
+    t.index %w[resource_type resource_id], name: 'index_roles_on_resource'
   end
 
-  create_table "suppliers", force: :cascade do |t|
-    t.string "name", limit: 30, default: "", null: false
-    t.string "supplier_address", default: "", null: false
-    t.string "phone_no", limit: 15, default: "", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'suppliers', force: :cascade do |t|
+    t.string 'name', limit: 30, default: '', null: false
+    t.string 'supplier_address', default: '', null: false
+    t.string 'phone_no', limit: 15, default: '', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "transactions", force: :cascade do |t|
-    t.integer "order_status", default: 0, null: false
-    t.bigint "order_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["order_id"], name: "index_transactions_on_order_id"
+  create_table 'transactions', force: :cascade do |t|
+    t.integer 'order_status', default: 0, null: false
+    t.bigint 'order_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['order_id'], name: 'index_transactions_on_order_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", limit: 30, default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "name", limit: 30, default: "", null: false
-    t.string "username", limit: 30, default: "", null: false
-    t.string "phone_no", limit: 15, default: "", null: false
-    t.string "image"
-    t.text "description"
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', limit: 30, default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'name', limit: 30, default: '', null: false
+    t.string 'username', limit: 30, default: '', null: false
+    t.string 'phone_no', limit: 15, default: '', null: false
+    t.string 'image'
+    t.text 'description'
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 
-  create_table "users_roles", id: false, force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "role_id"
-    t.index ["role_id"], name: "index_users_roles_on_role_id"
-    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
-    t.index ["user_id"], name: "index_users_roles_on_user_id"
+  create_table 'users_roles', id: false, force: :cascade do |t|
+    t.bigint 'user_id'
+    t.bigint 'role_id'
+    t.index ['role_id'], name: 'index_users_roles_on_role_id'
+    t.index %w[user_id role_id], name: 'index_users_roles_on_user_id_and_role_id'
+    t.index ['user_id'], name: 'index_users_roles_on_user_id'
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "category_products", "categories"
-  add_foreign_key "category_products", "products"
-  add_foreign_key "order_items", "orders"
-  add_foreign_key "order_items", "products"
-  add_foreign_key "orders", "users"
-  add_foreign_key "product_suppliers", "products"
-  add_foreign_key "product_suppliers", "suppliers"
-  add_foreign_key "products", "brands"
-  add_foreign_key "products", "users"
-  add_foreign_key "transactions", "orders"
+  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
+  add_foreign_key 'active_storage_variant_records', 'active_storage_blobs', column: 'blob_id'
+  add_foreign_key 'category_products', 'categories'
+  add_foreign_key 'category_products', 'products'
+  add_foreign_key 'order_items', 'orders'
+  add_foreign_key 'order_items', 'products'
+  add_foreign_key 'orders', 'users'
+  add_foreign_key 'product_suppliers', 'products'
+  add_foreign_key 'product_suppliers', 'suppliers'
+  add_foreign_key 'products', 'brands'
+  add_foreign_key 'transactions', 'orders'
 end
