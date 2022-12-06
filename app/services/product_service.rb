9e59713dc2c53658c4bd47416b7a011a34ec94c6
Hammad538 <hammad.rashid@devsinc.com>
2022-12-06 20:45:14 +0500

    Service:: Feedback addressed related to service

--- app/services/product_service.rb
+++ app/services/product_service.rb
@@ -1,11 +1,11 @@
 class ProductService
-  def remove_empty_array_elements(category_ids, supplier_ids)
+  def self.remove_empty_array_elements(category_ids, supplier_ids)
     @product_category = category_ids.reject { |e| e.nil? || e&.empty? }
     @product_supplier = supplier_ids.reject { |e| e.nil? || e&.empty? }
     [@product_category, @product_supplier]
   end
 
-  def assign_categories_and_supplier_to_product(category_ids, supplier_ids, flash)
+  def self.assign_categories_and_supplier_to_product(category_ids, supplier_ids, flash)
     remove_empty_array_elements(category_ids, supplier_ids) if category_ids.present? && supplier_ids.present?
     if @product_category.present? && @product_supplier.present?
       @product_category.each do |_product_category|
