4ef4809791e86572087b550006e19a89c17133cb
Hammad538 <hammad.rashid@devsinc.com>
2022-12-08 21:25:57 +0500

    Handled views and drop down for filters

--- app/javascript/packs/reports.js
+++ app/javascript/packs/reports.js
@@ -1,13 +1,10 @@
-
-
-$(document).on("change","#reports_dropdown",function() 
-{
-    alert("sdbfkjdsbfj,dsbn,fmnad,mn")
-    var state = $(this).val();
-    var request = $.ajax({
+$(document).on("change", "#reports_dropdown", function () {
+  alert("sdbfkjdsbfj,dsbn,fmnad,mn");
+  var state = $(this).val();
+  var request = $.ajax({
     url: "/reports",
     type: "get",
-    data: {reports : state},
-     dataType: "json"
-  })
+    data: { reports: state },
+    dataType: "json",
+  });
 });
