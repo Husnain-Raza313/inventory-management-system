d120772763c57d72495ab0a3bf6473c11b584466
Hammad538 <hammad.rashid@devsinc.com>
2022-12-09 18:49:41 +0500

    Reports:: Added Controller for reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,12 +1,6 @@
 class ReportsController < ApplicationController
   def index
-    @product = if params[:reports].present? && params[:reports] == 'Yearly'
-                 Product.group_by_year(:created_at).count
-               elsif params[:reports].present? && params[:reports] == 'Monthly'
-                 Product.group_by_month(:created_at).count
-               else
-                 Product.group_by_day(:created_at).count
-               end
+    @product = SalesAnalytics.new(params[:reports]).call if params[:reports].present?
     respond_to do |format|
       format.js {}
       format.html {}
