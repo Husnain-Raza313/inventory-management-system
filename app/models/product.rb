d3c218c280c516f0bbb2b7bfbc3b469a8d4998d5
Hammad538 <hammad.rashid@devsinc.com>
2022-12-09 18:53:34 +0500

    Validation:: Validations issue resolved

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ApplicationRecord
   validates :name, :image, :description, :serial_no, :bulk_price, :price_per_unit, :retail_price, presence: true
   validates :serial_no, :description, length: { minimum: 10, maximum: 30 }
   validates :bulk_price, numericality: { greater_than: :price_per_unit }
-  validates :price_per_unit, numericality: { greater_than: :retail_price }
+  validates :retail_price, numericality: { greater_than: :price_per_unit }
   validates :image, presence: true,
                     blob: { content_type: ALLOWED_IMAGE_TYPES, size_range: ALLOWED_IMAGE_SIZE }
 
