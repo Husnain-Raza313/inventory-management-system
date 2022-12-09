cf18408dc1006dfd8efac3955f7067ac185febb9
Hammad538 <hammad.rashid@devsinc.com>
2022-12-09 18:58:15 +0500

    Queries:: Added neccessary queries of reports

--- app/queries/sales_analytics.rb
+++ app/queries/sales_analytics.rb
@@ -8,7 +8,7 @@ class SalesAnalytics
   def call
     if @reports == I18n.t('yearly')
       Product.group_by_year(:created_at).count
-    elsif @reports == I18n.t('yearly')
+    elsif @reports == I18n.t('monthly')
       Product.group_by_month(:created_at).count
     else
       Product.group_by_day(:created_at).count
