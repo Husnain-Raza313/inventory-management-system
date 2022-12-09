415f297616b5fe1ffc69aa68bcb6b1b08fb61006
Hammad538 <hammad.rashid@devsinc.com>
2022-12-09 18:43:14 +0500

    Reprots:: Designed views for reports

--- app/javascript/packs/reports.js
+++ app/javascript/packs/reports.js
@@ -1,10 +1,9 @@
 $(document).on("change", "#reports_dropdown", function () {
-  alert("sdbfkjdsbfj,dsbn,fmnad,mn");
   var state = $(this).val();
-  var request = $.ajax({
+  $.ajax({
     url: "/reports",
     type: "get",
     data: { reports: state },
-    dataType: "json",
+    dataType: "script",
   });
 });
