556b2ae63c4c79dc2e64f41f75be99ba7185e5c3
Hammad538 <hammad.rashid@devsinc.com>
2022-12-12 15:29:20 +0500

    Reports:: Feedback addressed and added default option for dropdown

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,10 +1,6 @@
 class ReportsController < ApplicationController
   def index
     @product = SalesAnalytics.new(params[:reports]).call if params[:reports].present?
-    respond_to do |format|
-      format.js {}
-      format.html {}
-    end
   end
 
   def analytics
