85d4175be38f23fb928300174b4e8cc797bffaf3
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2022-12-12 22:13:45 +0500

    Application: Added Root Route for Cashier

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'order', to: 'order#show'
-  get 'orders', to: 'order#index'
+
   resources :order_items
   resources :categories
   resources :products
@@ -15,9 +14,10 @@ Rails.application.routes.draw do
     root to: 'home#index', as: :admin_route
   end
   authenticated :user, ->(u) { u.has_role?(:cashier) } do
-    root to: 'home#index', as: :user_route
+    root to: 'order#index', as: :user_route
   end
   root to: 'home#index'
+  get 'order', to: 'order#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   match ':status', to: 'application#page_not_found', via: :all
   get 'products/:id/analytics/:type', to: 'reports#analytics', as: :analytics
