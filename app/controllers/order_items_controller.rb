0349f60e26436538de042c502e98e4ed1974dc8a
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2022-12-12 22:34:30 +0500

    OrderItemsController: Added Flash messages

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -4,25 +4,34 @@ class OrderItemsController < ApplicationController
   def create
     user = current_user
     @order_item = @order.order_items.new(order_params)
-    p "jsk#{@order_item.quantity}"
-    p @order.user_id
     unless @order.save
-      p "jsk #{@order.errors.full_messages.to_sentence}"
+      flash[:error] = @order.errors.full_messages.to_sentence
     end
+
     session[:order_id] = @order.id
   end
 
   def update
     @order_item = @order.order_items.find(params[:id])
-    @order_item.update(order_params)
-    @order_items = current_order.order_items
+    if @order_item.update(order_params)
+      flash[:success] = t('update.success', param: 'Order Item')
+      @order_items = current_order.order_items
+    else
+      flash[:warning] = @product.errors.full_messages.to_sentence
+    end
+
     redirect_to order_path
   end
 
   def destroy
     @order_item = @order.order_items.find(params[:id])
-    @order_item.destroy
-    @order_items = current_order.order_items
+    if @order_item.destroy
+      flash[:success] = t('destroy.success', param: 'Order Item')
+      @order_items = current_order.order_items
+    else
+      flash[:error] = @product.errors.full_messages.to_sentence
+    end
+
     redirect_to order_path
   end
 
