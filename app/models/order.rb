e89bd3dba71d61171d7de22edd3b38fb247e0579
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2022-12-13 14:56:35 +0500

    Application: Added methods for total price in models

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,16 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :products, through: :order_items
   has_many :order_items, dependent: :destroy
+
+  before_save :set_total
+
+  def total
+    order_items.collect {|order_item| order_item.valid? ? order_item.total_price : 0}.sum
+  end
+
+  private
+
+  def set_total
+    self[:total_price] = total
+  end
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,4 +3,16 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
+
+  before_save :set_total_price
+
+  def total_price
+    self.product.retail_price * self.quantity
+  end
+
+  private
+
+  def set_total_price
+    self[:total_price] = total_price
+  end
 end
