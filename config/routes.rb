21b94f2c4ed998de83bb404974e55814d0ab725c
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2022-12-13 17:20:52 +0500

    Application: Specified Routes for OrderItems

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :order_items
+  resources :order_items, only: %i[create update destroy]
   resources :categories
   resources :products
   resources :home, only: %i[index]
