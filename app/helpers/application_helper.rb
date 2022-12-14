691b18f41d7e629e3493564b728b71a14fac23d1
Hammad538 <hammad.rashid@devsinc.com>
2022-12-14 18:41:26 +0500

    UI:: Resolved issues of UI

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,11 +4,11 @@ module ApplicationHelper
   IMAGE_SIZE = '500x100'
   def image_view(obj,image_size=nil)
     if obj.image.attached?
-      return image_tag(obj.image, size: !image_size.present? ? ApplicationHelper::IMAGE_SIZE : image_size[:image], alt: t('image', param: obj.class.name.to_s),
+      return image_tag(obj.image, size: image_size || ApplicationHelper::IMAGE_SIZE, alt: t('image', param: obj.class.name.to_s),
                                   class: ' w-100 rounded-top')
     end
 
-    image_tag('no-image.jpeg', size: !image_size.present? ? ApplicationHelper::IMAGE_SIZE : image_size[:image], alt: t('image', param: obj.class.name.to_s),
+    image_tag('no-image.jpeg', size: image_size || ApplicationHelper::IMAGE_SIZE, alt: t('image', param: obj.class.name.to_s),
                                class: ' w-100 rounded-top')
   end
 end
