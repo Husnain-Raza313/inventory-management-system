fb1908fe008c3286c31ef4f74994496a327c471f
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2022-12-15 22:05:48 +0500

    OrderItemController: Added flash message for update action

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -9,6 +9,12 @@ class OrderItemsController < ApplicationController
 
   def update
     session[params[:product_id]] = params[:quantity].to_i
+    if session[params[:product_id]].nil?
+      flash[:error] = t('error-message')
+    else
+      set_order_item('destroy')
+    end
+
     redirect_to orders_path
   end
 
