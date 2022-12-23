3a6784303560557c8d0be2148c35f1a988b5f121
Hammad538 <hammad.rashid@devsinc.com>
2022-12-23 18:49:21 +0500

    Filters:: Implemented filters and pagination

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,7 @@ class ProductsController < ApplicationController
   before_action :set_brand, only: %i[create]
 
   def index
-    @products = Product.paginate(page: params[:page], per_page: 10).all
+    @products = Product.paginate(page: params[:page], per_page: 10).available_products
     return unless params[:brand_ids].present? || params[:category_ids].present?
 
     @products = ProductService.new(category_ids: params[:category_ids], brand_ids: params[:brand_ids],
