c55e24e325c2a70ca132aa4c6a9d3b2c70221c32
Hammad538 <hammad.rashid@devsinc.com>
2022-12-23 16:39:55 +0500

    Filters:: Completed Filters for app

--- app/services/product_service.rb
+++ app/services/product_service.rb
@@ -1,17 +1,26 @@
 # frozen_string_literal: true
 
 class ProductService < ApplicationService
-  attr_reader :product, :product_category_ids, :product_supplier_ids
+  attr_reader :product, :product_category_ids, :product_supplier_ids, :brand_ids, :products
 
   def initialize(**args)
     super
-    @product = params[:brand].products.new(params[:product_params])
+    if params[:brand].present?
+      @product = params[:brand].products.new(params[:product_params])
+      @product_supplier_ids = params[:supplier_ids].drop(1)
+    else
+      @brand_ids = params[:brand_ids].drop(1)
+      @products = params[:products]
+    end
     @product_category_ids = params[:category_ids].drop(1)
-    @product_supplier_ids = params[:supplier_ids].drop(1)
   end
 
   def execute
     assign_categories_and_supplier_to_product
+  end
+
+  def filter
+    search_brands_and_categories
   end
 
   private
@@ -31,5 +40,12 @@ class ProductService < ApplicationService
     else
       { product: nil, message: I18n.t('select_category_and_supplier') }
     end
+  end
+
+  def search_brands_and_categories
+    @products = @products.where(brand_id: brand_ids) if brand_ids.present?
+    return @products unless product_category_ids.present?
+
+    @products = @products.joins(:category_products).where(category_products: { category_id: product_category_ids })
   end
 end
