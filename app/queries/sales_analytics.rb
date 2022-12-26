baf0bd209928a098554a6ca1b848f4b12e9008bb
Hammad538 <hammad.rashid@devsinc.com>
2022-12-26 18:24:42 +0500

    Charts:: Resolved charts title issue

--- app/queries/sales_analytics.rb
+++ app/queries/sales_analytics.rb
@@ -2,12 +2,13 @@
 
 class SalesAnalytics < ApplicationQueries
   def call
-    if params[:reports] == I18n.t('yearly')
-      Product.group_by_year(:created_at).count
-    elsif params[:reports] == I18n.t('monthly')
-      Product.group_by_month(:created_at).count
-    else
-      Product.group_by_day(:created_at).count
-    end
+    @product = if params[:reports] == I18n.t('yearly')
+                 Product.group_by_year(:created_at).count
+               elsif params[:reports] == I18n.t('monthly')
+                 Product.group_by_month(:created_at).count
+               else
+                 Product.group_by_day(:created_at).count
+               end
+    @product = @product.transform_keys { |key| key.strftime('%b %d, %Y') } if @product.present?
   end
 end
