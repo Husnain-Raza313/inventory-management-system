4302b00636b8df8856e3d9716602f11e9033f93f
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2022-12-27 16:36:33 +0500

    Application: Changed Requests To Ajax for Update and Remove of Order Items -> Created Partials -> Refactored some OrderItems Controller Code -> Added JS ERB files

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,25 +1,31 @@
 # frozen_string_literal: true
 
 class OrderItemsController < ApplicationController
+
   def create
-    session[:order_array], session[:order_quantity][params[:product_id]], message, type = OrderItemService.new(order_params: params,
+    session[:order_array], session[:order_quantity][params[:product_id]], @message, @type = OrderItemService.new(order_params: params,
                                                                                                                session: session).execute
     return unless params[:request] != 'create'
 
-    flash[type] = message
-    redirect_back(fallback_location: root_path)
+    set_products
+    render 'update.js.erb'
   end
 
   def destroy
     if session[:order_array].delete(params[:id])
-      message = t('destroy.success', param: 'Order Item')
-      type = 'success'
+      @message = t('destroy.success', param: 'Order Item')
+      @type = 'success'
     else
-      message = t('error-message')
-      type = 'error'
+      @message = t('error-message')
+      @type = 'error'
     end
 
-    flash[type] = message
-    redirect_to orders_path(status: 'order-list')
+    set_products
+  end
+
+  private
+
+  def set_products
+    @products = Product.ordered_session_products(session[:order_array])
   end
 end
